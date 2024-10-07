acaeda4cfe855a898f09c6b6f94ecd7ec516e2aa
Maks0s <maks0sprogram@gmail.com>
2024-10-07 17:14:20 +0300

    feat[Shortly.Application]: Creat 'AddShortUrlCommandValidator'
    - In addition creat a custom 'UrlValidation' for more convenient URL validation

---
+++ src/Shortly.Application/ShortUrls/CQRS/Commands/Add/AddShortUrlCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Shortly.Application.ShortUrls.CustomeValidators;
+
+namespace Shortly.Application.ShortUrls.CQRS.Commands.Add
+{
+    public class AddShortUrlCommandValidator
+        : AbstractValidator<AddShortUrlCommand>
+    {
+        public AddShortUrlCommandValidator()
+        {
+            RuleFor(asuc => asuc.OriginalUrl)
+                .ShouldBeValidUrl();
+        }
+    }
+}
---
+++ src/Shortly.Application/ShortUrls/CustomeValidators/UrlValidation.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Shortly.Application.ShortUrls.CustomeValidators
+{
+    public static class UrlValidation
+    {
+        public static IRuleBuilderOptions<T, string> ShouldBeValidUrl<T>(
+                this IRuleBuilderInitial<T, string> ruleBuilder
+            )
+        {
+            return ruleBuilder
+                .NotNull()
+                .NotEmpty()
+                .Must(IsValidUrl).WithMessage("Invalid URL format.");
+        }
+
+        private static bool IsValidUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out _);
+        }
+    }
+}
