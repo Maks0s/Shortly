7232270543022550e38e526c7d8b2eac1eff2a27
Maks0s <maks0sprogram@gmail.com>
2024-10-07 11:13:11 +0300

    feat[Shortly.Presentation]: Add 'BaseApiController'
    Add 'BaseApiController' inherited from 'ControllerBase' and implemente multiple errors handling

---
+++ src/Shortly.Presentation/Controllers/Common/BaseApiController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc;
+using ErrorOr;
+
+namespace Shortly.Presentation.Controllers.Common
+{
+    [Controller]
+    public class BaseApiController : ControllerBase
+    {
+        protected ActionResult Problem(List<Error> errors)
+        {
+            if (errors.TrueForAll(error => error.Code.Equals("Validation failure")))
+            {
+                var msd = new ModelStateDictionary();
+
+                foreach (var error in errors)
+                {
+                    var invalidFieldAndDescription = error.Description.Split('-');
+                    msd.AddModelError(
+                            invalidFieldAndDescription[0],
+                            invalidFieldAndDescription[1]
+                        );
+                }
+
+                return ValidationProblem(msd);
+            }
+
+            var firstOccurredError = errors[0];
+
+            return Problem(
+                    statusCode: firstOccurredError.NumericType,
+                    title: firstOccurredError.Code,
+                    detail: firstOccurredError.Description,
+                    instance: Request.Path.Value
+                );
+        }
+    }
+}
