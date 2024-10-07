8e889fe569309fe81fe418db240918eaacec2885
Maks0s <maks0sprogram@gmail.com>
2024-10-07 11:02:29 +0300

    feat[Shortly]: Build Clean Architecture skeleton
    - Create layers for usability and abstraction
    - Instead of directly injecting services in 'Program.cs' in 'builder.Services', replaced it with injecting services in the 'DependencyInjection' classes of the app layers via '.Add[LayerName]', and after using '.Add[LayerName]' on 'builder.Services' in 'Program.cs'

---
+++ src/Shortly.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shortly.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+                this IServiceCollection services,
+                IConfiguration configuration
+            )
+        {
+            services.AddPersistence(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddPersistence(
+                this IServiceCollection services,
+                IConfiguration configuration
+            )
+        {
+            return services;
+        }
+    }
+}
--- src/Shortly.Presentation/Program.cs
+++ src/Shortly.Presentation/Program.cs
@@ -1,9 +1,12 @@
+using Shortly.Application;
+using Shortly.Infrastructure;
+using Shortly.Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddPresentation();
 
 var app = builder.Build();
 
