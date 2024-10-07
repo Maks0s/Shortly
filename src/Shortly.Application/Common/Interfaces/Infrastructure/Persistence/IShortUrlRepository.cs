6939e6274dbce668f23ea43a07fde674e000d7b1
Maks0s <maks0sprogram@gmail.com>
2024-10-07 15:16:39 +0300

    feat[Shortly.Application]: Create 'IShortUrlRepository'
    - Create 'AddShortUrlAsync' method signature
    - Create 'GetAllUrlsAsync' method signature
    - Create 'GetOriginalUrlAsync' method signature
    - Create 'DeleteUrlAsync' method signature

---
+++ src/Shortly.Application/Common/Interfaces/Infrastructure/Persistence/IShortUrlRepository.cs
@@ -0,0 +1,12 @@
+﻿using Shortly.Domain.Entities;
+
+namespace Shortly.Application.Common.Interfaces.Infrastructure.Persistence
+{
+    public interface IShortUrlRepository
+    {
+        public Task<ShortUrl?> AddShortUrlAsync(ShortUrl shortUrlToAdd);
+        public Task<List<ShortUrl>> GetAllUrlsAsync();
+        public Task<string?> GetOriginalUrlAsync(string shortenedUrlKey);
+        public Task<int> DeleteUrlAsync(string shortenedUrlKey);
+    }
+}
