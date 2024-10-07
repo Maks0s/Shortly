aaa70d510e14bf2761140e3dbbf493cc56adea7c
Maks0s <maks0sprogram@gmail.com>
2024-10-07 20:05:16 +0300

    feat[Shortly.Presentation]: Create 'ShortUrlsController' and 'AddShortUrl' endpoint

---
+++ src/Shortly.Presentation/Controllers/ShortUrls/ShortUrlsController.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shortly.Presentation.Common.DTOs.UrlDTOs;
+using Shortly.Presentation.Common.Mappers;
+using Shortly.Presentation.Controllers.Common;
+
+namespace Shortly.Presentation.Controllers.ShortUrls
+{
+    [Route("/")]
+    public class ShortUrlsController : BaseApiController
+    {
+        private readonly ISender _sender;
+
+        private readonly UrlMapper _mapper;
+
+        public ShortUrlsController(ISender sender, UrlMapper mapper)
+        {
+            _sender = sender;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddShortUrl([FromBody] UrlRequest urlRequest)
+        {
+            var addCommand = _mapper.MapToAddShortUrlCommand(urlRequest);
+
+            var addResult = await _sender.Send(addCommand);
+
+            return addResult.Match(
+                    added => Created(
+                            //TODO: Refactor to CreateAtAction
+                            // after creating GetOriginalUrl
+                            "mock",
+                            _mapper.MapToUrlResponse(added)
+                        ),
+                    errors => Problem(errors)
+                );
+        }
+    }
+}
