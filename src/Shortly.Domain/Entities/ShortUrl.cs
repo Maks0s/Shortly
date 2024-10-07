67921dad693204ee729d3b92fdd482728ef5ee22
Maks0s <maks0sprogram@gmail.com>
2024-10-07 11:54:24 +0300

    feat[Shortly.Domain]: Add domain model 'ShortUrl'

---
+++ src/Shortly.Domain/Entities/ShortUrl.cs
@@ -0,0 +1,10 @@
+﻿namespace Shortly.Domain.Entities
+{
+    public class ShortUrl
+    {
+        public string ShortenedUrlKey { get; set; } = default!;
+        public string OriginalUrl { get; set; } = default!;
+        public DateTime CreationDate { get; set; }
+        public int TransitionCount { get; set; }
+    }
+}
