75ee456a1906b43488d4a36b1a6cf1d02e90cf6e
Maks0s <maks0sprogram@gmail.com>
2024-10-08 16:10:28 +0300

    feat[Shortly.Application]: Create 'DeleteShortUrlCommand' and its handling
    - Create 'DeleteShortUrlCommand' and 'DeleteShortUrlCommandHandler'

---
+++ src/Shortly.Application/ShortUrls/CQRS/Commands/Delete/DeleteShortUrlCommand.cs
@@ -0,0 +1,9 @@
+﻿using Shortly.Application.Common.Interfaces.Application.CQRS;
+using Shortly.Domain.Entities;
+
+namespace Shortly.Application.ShortUrls.CQRS.Commands.Delete
+{
+    public record DeleteShortUrlCommand(
+            string ShortenedUrlKey
+        ) : ICommand<ErrorOr.Deleted>;
+}
---
+++ src/Shortly.Application/ShortUrls/CQRS/Commands/Delete/DeleteShortUrlCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using ErrorOr;
+using Shortly.Application.Common.AppErrors;
+using Shortly.Application.Common.Interfaces.Application.CQRS;
+using Shortly.Application.Common.Interfaces.Infrastructure.Persistence;
+
+namespace Shortly.Application.ShortUrls.CQRS.Commands.Delete
+{
+    public class DeleteShortUrlCommandHandler
+        : ICommandHandler<DeleteShortUrlCommand, ErrorOr.Deleted>
+    {
+        private readonly IShortUrlRepository _shortUrlRepository;
+
+        public DeleteShortUrlCommandHandler(IShortUrlRepository shortUrlRepository)
+        {
+            _shortUrlRepository = shortUrlRepository;
+        }
+
+        public async Task<ErrorOr<Deleted>> Handle(
+                DeleteShortUrlCommand command, 
+                CancellationToken cancellationToken
+            )
+        {
+            var deletedUrlsCount =
+                await _shortUrlRepository.DeleteUrlAsync(command.ShortenedUrlKey);
+
+            if (deletedUrlsCount < 1)
+            {
+                return Errors.Urls.NotFound();
+            }
+
+            return Result.Deleted;
+        }
+    }
+}
