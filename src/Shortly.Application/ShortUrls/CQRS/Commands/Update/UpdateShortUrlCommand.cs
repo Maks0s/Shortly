4b7955d7a6639687b4486628d5004bd16bf76e80
Maks0s <maks0sprogram@gmail.com>
2024-10-08 16:02:37 +0300

    feat[Shortly.Application]: Create 'UpdateShortUrlCommand' and its handling
    - Create 'UpdateShortUrlCommand' and 'UpdateShortUrlCommandHandler'

---
+++ src/Shortly.Application/ShortUrls/CQRS/Commands/Update/UpdateShortUrlCommand.cs
@@ -0,0 +1,9 @@
+﻿using Shortly.Application.Common.Interfaces.Application.CQRS;
+using Shortly.Domain.Entities;
+
+namespace Shortly.Application.ShortUrls.CQRS.Commands.Update
+{
+    public record UpdateShortUrlCommand(
+            string ShortenedUrlKey
+        ) : ICommand<ShortUrl?>;
+}
---
+++ src/Shortly.Application/ShortUrls/CQRS/Commands/Update/UpdateShortUrlCommandHandler.cs
@@ -0,0 +1,73 @@
+﻿using ErrorOr;
+using Microsoft.Extensions.Configuration;
+using Shortly.Application.Common.Interfaces.Application.CQRS;
+using Shortly.Application.Common.Interfaces.Application.Services;
+using Shortly.Application.Common.Interfaces.Infrastructure.Persistence;
+using Shortly.Domain.Entities;
+using Shortly.Application.Common.AppErrors;
+
+namespace Shortly.Application.ShortUrls.CQRS.Commands.Update
+{
+    public class UpdateShortUrlCommandHandler
+        : ICommandHandler<UpdateShortUrlCommand, ShortUrl?>
+    {
+        private readonly IUrlShorteningService _urlShorteningService;
+
+        private readonly IConfiguration _configuration;
+
+        private readonly IShortUrlRepository _shortUrlRepository;
+
+        public UpdateShortUrlCommandHandler(
+                IShortUrlRepository shortUrlRepository,
+                IUrlShorteningService urlShorteningService,
+                IConfiguration configuration
+            )
+        {
+            _shortUrlRepository = shortUrlRepository;
+            _urlShorteningService = urlShorteningService;
+            _configuration = configuration;
+        }
+
+        public async Task<ErrorOr<ShortUrl?>> Handle(
+                UpdateShortUrlCommand command, 
+                CancellationToken cancellationToken
+            )
+        {
+            var originalUrl =
+                await _shortUrlRepository
+                    .GetOriginalUrlAsync(command.ShortenedUrlKey);
+
+            if (string.IsNullOrEmpty(originalUrl))
+            {
+                return Errors.Urls.NotFound();
+            }
+
+            await _shortUrlRepository.DeleteUrlAsync(command.ShortenedUrlKey);
+
+            var newShortenedUrlKey =
+                await _urlShorteningService
+                    .GenerateShortenedUrlKeyAsync();
+
+            string shortenedUrl = $"{_configuration["BaseUrl"]}/{newShortenedUrlKey}";
+
+            var newUrlToAdd = new ShortUrl()
+            {
+                ShortenedUrlKey = newShortenedUrlKey,
+                OriginalUrl = originalUrl,
+                ShortenedUrl = shortenedUrl,
+                CreationDate = DateTime.UtcNow,
+                TransitionCount = 0
+            };
+
+            var updatedUrl =
+                await _shortUrlRepository.AddShortUrlAsync(newUrlToAdd);
+
+            if (updatedUrl is null)
+            {
+                return Errors.ServerDataManipulation.NotAdded();
+            }
+
+            return updatedUrl;
+        }
+    }
+}
